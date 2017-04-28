6802b72c33378cdfe6f5781aedad7d4036f8717d
Maga Abdurakhmanov <maqdev@gmail.com>
2017-04-28 22:00:10 +0200

    + test fixed

--- src/test/scala/com/hypertino/transport/resolvers/ConsulServiceResolverSpec.scala
+++ src/test/scala/com/hypertino/transport/resolvers/ConsulServiceResolverSpec.scala
@@ -5,21 +5,19 @@ import com.hypertino.hyperbus.transport.resolvers.PlainEndpoint
 import com.orbitz.consul.Consul
 import monix.eval.Task
 import monix.execution.Ack.Continue
-import monix.execution.Scheduler
-import monix.execution.cancelables.BooleanCancelable
-import monix.reactive.subjects.ConcurrentSubject
-import org.scalatest.{FlatSpec, Matchers}
-import org.scalatest.concurrent.ScalaFutures
+import monix.execution.atomic.AtomicInt
+import org.scalatest.concurrent.{Eventually, ScalaFutures}
 import org.scalatest.time.{Millis, Seconds, Span}
+import org.scalatest.{FlatSpec, Matchers}
 
 import scala.concurrent.duration._
 
-class ConsulServiceResolverSpec extends FlatSpec with ScalaFutures with Matchers {
+class ConsulServiceResolverSpec extends FlatSpec with ScalaFutures with Matchers with Eventually {
   val consul = Consul.builder().build()
   import monix.execution.Scheduler.Implicits.global
 
   implicit val defaultPatience =
-    PatienceConfig(timeout = Span(2, Seconds), interval = Span(100, Millis))
+    PatienceConfig(timeout = Span(4, Seconds), interval = Span(300, Millis))
 
   "Service" should "resolve" in {
     val agentClient = consul.agentClient
@@ -35,6 +33,15 @@ class ConsulServiceResolverSpec extends FlatSpec with ScalaFutures with Matchers
     agentClient.deregister("1")
   }
 
+  "Not existing service" should "fail fast" ignore {
+    val agentClient = consul.agentClient
+    val serviceName = "unknown-service"
+    val serviceId = "2"
+
+    val r = new ConsulServiceResolver(consul)
+    r.lookupService("test-service").runAsync.failed.futureValue shouldBe a[NoTransportRouteException]
+  }
+
   "Service" should "update on change" in {
     val agentClient = consul.agentClient
     val serviceName = "test-service"
@@ -42,19 +49,26 @@ class ConsulServiceResolverSpec extends FlatSpec with ScalaFutures with Matchers
 
     agentClient.register(15533, 3L, serviceName, serviceId)
     agentClient.pass(serviceId)
-    Thread.sleep(200)
 
+    val counter = AtomicInt(0)
     val r = new ConsulServiceResolver(consul)
 
-    Thread.sleep(200)
+    r.serviceObservable("test-service").subscribe(seq ⇒ {
+      val v = counter.incrementAndGet()
+      if (v == 1) {
+        agentClient.fail(serviceId)
+        seq should equal(Seq(PlainEndpoint("127.0.0.1", Some(15533))))
+      }
+      else {
+        seq shouldBe empty
+        agentClient.deregister("1")
+      }
+      Continue
+    })
 
-    val f = r.serviceObservable("test-service").take(2).toListL.runAsync
-
-    Task.eval {
-      //agentClient.fail(serviceId)
-      agentClient.deregister("1")
-    } delayExecution 150.milliseconds runAsync
-
-    f.futureValue should equal(Seq(Seq(PlainEndpoint("127.0.0.1", Some(15533))), Seq.empty))
+    eventually {
+      counter.get should equal(2)
+    }
+    agentClient.deregister("1")
   }
 }
