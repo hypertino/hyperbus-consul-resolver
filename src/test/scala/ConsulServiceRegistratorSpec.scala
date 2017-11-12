124e23e144ce222833aa8dd6fdfd44b2c0f11f0a
Maga Abdurakhmanov <maqdev@gmail.com>
2017-11-12 23:17:59 +0100

    + BSD -> MPLv2

--- src/test/scala/ConsulServiceRegistratorSpec.scala
+++ src/test/scala/ConsulServiceRegistratorSpec.scala
@@ -1,3 +1,11 @@
+/*
+ * Copyright (c) 2017 Magomed Abdurakhmanov, Hypertino
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ */
+
 import com.hypertino.hyperbus.model.Method
 import com.hypertino.hyperbus.transport.api.NoTransportRouteException
 import com.hypertino.hyperbus.transport.api.matchers.{RegexMatcher, RequestMatcher, Specific}
